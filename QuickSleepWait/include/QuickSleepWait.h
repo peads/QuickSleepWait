3b2c03917f0a4a6911c3261c14962ce159bb313c
Patrick Eads <peads@users.noreply.github.com>
2026-08-07 03:55:59 +0000

    Accidentally the whole lincense header

--- QuickSleepWait/include/QuickSleepWait.h
+++ QuickSleepWait/include/QuickSleepWait.h
@@ -1,3 +1,20 @@
+/*
+* This file is part of the QuickSleepWait distribution
+ * (https://github.com/peads/QuickSleepWait).
+ * Copyright (c) 2026 Patrick Eads.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef QSW_H
 #define QSW_H
 #include <Mod/CppUserModBase.hpp>
